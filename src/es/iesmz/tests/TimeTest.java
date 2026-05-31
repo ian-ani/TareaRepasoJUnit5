a77980db198ce35e26ee16b861cd072d86b90118
Iris <irisaunav@gmail.com>
2026-05-31 21:42:33 +0200

    Create TimeTest.java

---
+++ src/es/iesmz/tests/TimeTest.java
@@ -0,0 +1,19 @@
+package es.iesmz.tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TimeTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void nextSecondTest() {
+    }
+}
